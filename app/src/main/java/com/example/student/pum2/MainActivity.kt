7f178918992777e04fec06c639da8ff43df28dca
wani24 <34307715+wani24@users.noreply.github.com>
2017-12-20 12:05:07 +0100

    Final Commit

--- app/src/main/java/com/example/student/pum2/MainActivity.kt
+++ app/src/main/java/com/example/student/pum2/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.example.student.pum2
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        btnZmien.setOnClickListener{
+            val miasto = txtWpisz.text
+
+            txtMiasto.text = miasto
+
+        }
+        btnMiejsca.setOnClickListener{
+
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
+        }
     }
-}
+
+
+
+
+
+
