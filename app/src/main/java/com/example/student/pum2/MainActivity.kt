d77c651fb5054f6ffaec4f035f264b1005a76bae
wani24 <34307715+wani24@users.noreply.github.com>
2017-12-28 20:37:54 +0100

    Initial Commit

--- app/src/main/java/com/example/student/pum2/MainActivity.kt
+++ app/src/main/java/com/example/student/pum2/MainActivity.kt
@@ -7,7 +7,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 import android.os.Bundle
 import android.support.v4.app.BundleCompat
 import java.lang.Object
-
+import java.util.ArrayList;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 class MainActivity : AppCompatActivity() {
 
 
@@ -15,12 +17,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ButterKnife.bind(this);
 
         btnZmien.setOnClickListener{
             val miasto = txtWpisz.text
 
             txtMiasto.text = miasto
-            
+
 
         }
         btnMiejsca.setOnClickListener{
